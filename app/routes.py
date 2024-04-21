e31e1348a69941f45660e95e4164e77a734cc9d1
UwaProj <22029591@student.uwa.edu.au>
2024-04-21 12:43:08 +0800

    added registration. includes 'secret qns' for if we ever do acc recovery. if not needed, delete

--- app/routes.py
+++ app/routes.py
@@ -15,4 +15,8 @@ def test():
 @app.route('/login')
 def login():
     return render_template('login.html')
+@app.route('/')
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
 

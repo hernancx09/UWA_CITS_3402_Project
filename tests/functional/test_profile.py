75a35c396b659c1322e2efe3596dcf19ef84905b
sandoncj <60812025+sandoncj@users.noreply.github.com>
2024-05-07 23:00:52 +0800

    Updated Test
    Added more profile page tests regarding messages and deletion of posts

--- tests/functional/test_profile.py
+++ tests/functional/test_profile.py
@@ -1,10 +1,10 @@
 import datetime
 from flask_login import current_user, login_user
-from app.models import Posts, Users
+from app.models import Messages, Posts, Users
 from app.db_helpers import db
 
 
-def test_profile_page(client, app_functional):
+def test_profile_page(client, app_functional, new_user):
     """
     GIVEN a test client
     WHEN the user logs in and navigates to profile page
@@ -15,7 +15,13 @@ def test_profile_page(client, app_functional):
         email = "jimmy@gmail.com",
         password = "Password1",
         passwordCheck = "Password1"), follow_redirects=True)
-
+    
+    client.post('/registration', data= dict(
+        name = "Abi",
+        email = "abi@gmail.com",
+        password = "Password1",
+        passwordCheck = "Password1"), follow_redirects=True)
+    
     with app_functional.app_context():
         post = Posts(
             author = Users.query.first(),
@@ -31,10 +37,19 @@ def test_profile_page(client, app_functional):
         )
         db.session.add(post)
         db.session.commit
+        
+        message = Messages(
+            job_id = Posts.query.first().id,
+            applicant_id = Users.query.filter(Users.name == "Abi").first().id,
+            employer_id = Users.query.filter(Users.name == "Jimmy").first().id,
+            message = "Hi Jimmy"
+        )
+        db.session.add(message)
+        db.session.commit
             
         with app_functional.test_request_context():
             login_user(Users.query.first())
-        
+
             response = client.get('/profile')
         
             assert response.status_code == 200
@@ -43,3 +58,17 @@ def test_profile_page(client, app_functional):
             assert b"My Applications" in response.data
             assert b"Messages" in response.data
             assert b"test_job" in response.data
+            assert b"abi@gmail.com" in response.data
+            
+            client.post('/profile', data = dict(
+                post_id = Posts.query.first().id))
+
+            response = client.get('/profile')
+            
+            assert response.status_code == 200
+            assert response.request.path == "/profile"
+            assert b"My Ongoing Jobs" in response.data
+            assert b"My Applications" in response.data
+            assert b"Messages" in response.data
+            assert b"test_job" not in response.data
+            assert b"abi@gmail.com" not in response.data
